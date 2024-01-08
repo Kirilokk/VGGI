73969d2317857f91d499fe81be895ae8aaa3a9b6
Kirilokk <kiriloki2018@gmail.com>
2024-01-08 10:03:25 +0200

    Init commit for CGW

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let R1 = 0.35;                   // Radius of smaller cylinder
 let R2 = 3 * R1;                // Radius of bigger cylinder
 let b =  3 * R1;                // Height of the surface
 let stepAlpha = 0.1             // Step for alpha
-let stepBeta = 10               // Step for beta
+let stepBeta = 1               // Step for beta
 
 let horizontals = 0;
 let verticals = 0;
@@ -18,7 +18,6 @@ let verticals = 0;
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
-
 
 // Update parameters on user change
 function setParameters(R1New, R2New, BNew, alphaNew, betaNew){
@@ -33,18 +32,7 @@ function setParameters(R1New, R2New, BNew, alphaNew, betaNew){
     verticals = 0;
 }
 
-// Function to set default surface parameters
-function setDefault() {
-    const r1 = 0.35                                               
-    const r2 = 1.05
-    const b = 1.05
-    const alphaStep = 0.1;
-    const betaStep = 10;
-
-
-    setParameters(r1, r2, b, alphaStep, betaStep)
-
-    surface.BufferData(CreateSurfaceData());
+function updateHtml(r1, r2, b, alphaStep, betaStep){
     document.getElementById("R1Current").textContent = r1;
     document.getElementById("R2Current").textContent = r2;
     document.getElementById("BCurrent").textContent = b;
@@ -56,21 +44,52 @@ function setDefault() {
     document.getElementById("paramB").value = b
     document.getElementById("paramAlphaStep").value = alphaStep;
     document.getElementById("paramBetaStep").value = betaStep;
+}
+
+// Function to set default surface parameters
+function setDefault() {
+    const r1 = 0.35                                               
+    const r2 = 1.05
+    const b = 1.05
+    const alphaStep = 0.1;
+    const betaStep = 1;
+
+    setParameters(r1, r2, b, alphaStep, betaStep)
+    updateHtml(r1, r2, b, alphaStep, betaStep)
+    surface.BufferData(CreateSurfaceData());
     draw();
 }
+
+// Function to update the surface with the new max value of parameter r
+function updateParameters() {
+    const r1 = parseFloat(document.getElementById("paramR1").value);
+    const r2 = parseFloat(document.getElementById("paramR2").value);
+    const b = parseFloat(document.getElementById("paramB").value);
+    const alphaStep = parseFloat(document.getElementById("paramAlphaStep").value);
+    const betaStep = parseFloat(document.getElementById("paramBetaStep").value);
+    
+    setParameters(r1, r2, b, alphaStep, betaStep)
+
+    updateHtml(r1, r2, b, alphaStep, betaStep)
+    surface.BufferData(CreateSurfaceData());
+    draw();
+}
+
 // Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
-    this.verticesLength = 0;
 
-    this.BufferData = function(vertices) {
+    this.BufferData = function(vertices, normals) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STREAM_DRAW);
 
-        this.verticesLength  = vertices.length;
+        this.count = vertices.length/3;
     }
 
     this.Draw = function() {
@@ -79,15 +98,11 @@ function Model(name) {
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-        // Drawing horizontal lines
-        for (let i = 0; i < horizontals; i++ ) {
-            gl.drawArrays(gl.LINE_STRIP, verticals * i, verticals);
-        }
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
-        // Drawing vertical lines
-        for (let i = 0; i < verticals; i++ ) {
-            gl.drawArrays(gl.LINE_STRIP, this.verticesLength / 6 + (horizontals * i), horizontals);
-        }
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
 }
 
@@ -100,10 +115,15 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
+    this.iAttribNormal = -1;
     // Location of the uniform specifying a color for the primitive.
     this.iColor = -1;
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
+
+    this.iWorldInverseTransposeLocation =  -1;
+    this.iLightWorldPositionLocation = -1;
+    this.iWorldLocation = -1;
 
     this.Use = function() {
         gl.useProgram(this.prog);
@@ -120,7 +140,7 @@ function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
+    let projection = m4.perspective(Math.PI/6, 1, 8, 12); 
     
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
@@ -135,78 +155,90 @@ function draw() {
        combined transformation matrix, and send that to the shader program. */
     let modelViewProjection = m4.multiply(projection, matAccum1 );
 
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    let worldInverseMatrix = m4.inverse(matAccum1);
+    let worldInverseTransposeMatrix = m4.transpose(worldInverseMatrix);
 
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
+    gl.uniformMatrix4fv(shProgram.iWorldInverseTransposeLocation, false, worldInverseTransposeMatrix);
+    gl.uniformMatrix4fv(shProgram.iWorldLocation, false, matAccum1);
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
+    gl.uniform3fv(shProgram.iLightWorldPositionLocation, [20, 30, 50]);
     surface.Draw();
 }
 
-// Function to calculate r value for the surface according to the formula
-function calculateValue(alpha) {
-    const sinResult = Math.sin(deg2rad(180 * alpha) / (4 * b)) * Math.sin(deg2rad(180 * alpha) / (4 * b));
-    return (R2 - R1) * sinResult + R1;
-  }
-
-// Function to update the surface with the new max value of parameter r
-function updateParameters() {
-    const r1 = parseFloat(document.getElementById("paramR1").value);
-    const r2 = parseFloat(document.getElementById("paramR2").value);
-    const b = parseFloat(document.getElementById("paramB").value);
-    const alphaStep = parseFloat(document.getElementById("paramAlphaStep").value);
-    const betaStep = parseFloat(document.getElementById("paramBetaStep").value);
-    
-    setParameters(r1, r2, b, alphaStep, betaStep)
-
-
-    surface.BufferData(CreateSurfaceData());
-    document.getElementById("R1Current").textContent = r1;
-    document.getElementById("R2Current").textContent = r2;
-    document.getElementById("BCurrent").textContent = b;
-    document.getElementById("AlphaCurrent").textContent = alphaStep;
-    document.getElementById("BetaCurrent").textContent = betaStep;
-    draw();
+// Function to calculate X surface coordinate
+function getX (alpha, beta){
+    let r = ( R2 - R1 ) * Math.pow(Math.sin(deg2rad(( 180 * alpha ) / (4 * b))),2) + R1; 
+    return r * Math.cos(deg2rad(beta));
 }
 
+// Function to calculate Y surface coordinate
+function getY (alpha, beta){
+    let r = ( R2 - R1 ) * Math.pow(Math.sin(deg2rad(( 180 * alpha ) / (4 * b))),2) + R1; 
+    return r * Math.sin(deg2rad(beta))
+}
+
+// Function to calculate Z surface coordinate
+function getZ(alpha){
+    return alpha;
+}
+
+// Partial derivatives for U
+function getDerivativeU(u, v, x, y, z, delta){
+    let dx_du = (getX(u + delta, v) - x) / deg2rad(delta);  
+    let dy_du = (getY(u + delta, v) - y) / deg2rad(delta);
+    let dz_du = (getZ(u + delta, v) - z) / deg2rad(delta);
+
+    return [dx_du, dy_du, dz_du];
+}
+
+// Partial derivatives for V
+function getDerivativeV(u, v, x, y, z, delta){
+    let dx_dv = (getX(u, v + delta) - x) / deg2rad(delta);  
+    let dy_dv = (getY(u, v + delta) - y) / deg2rad(delta);
+    let dz_dv = (getZ(u, v + delta) - z) / deg2rad(delta);
+
+    return [dx_dv, dy_dv, dz_dv];
+}
 
 function CreateSurfaceData()
 {
-    // console.log(R1)
-    // console.log(R2)
-    // console.log(b)
     let vertexList = [];
-
+    let normalsList = [];
     let x = 0
     let y = 0
     let z = 0
-
+    let delta = 0.0001
     // Ranges:
     // 0 <= alpha(i) <= 2b
     // 0 <= beta(j) <= 2PI
-    for (let i = 0;  i<= 2 * b;  i+= stepAlpha) {
-        for (let j = 0; j<= 360; j+=stepBeta){
-            x = calculateValue(i) * Math.cos(deg2rad(j));
-            y = calculateValue(i) * Math.sin(deg2rad(j));
-            z = i;
+    for (let i = 0;  i <= 2 * b;  i+= stepAlpha) {
+        for (let j = 0; j <= 360; j+=stepBeta){
+
+            x = getX(i, j);
+            y = getY(i, j);
+            z = getZ(i);
+            let derU = getDerivativeU(i, j, x, y, z, delta);
+            let derV = getDerivativeV(i, j, x, y, z, delta);
+            let UVproduct = m4.cross(derU, derV);
             
             vertexList.push(x, y, z);
-        }
-        horizontals++;
-    }
+            normalsList.push(UVproduct[0], UVproduct[1], UVproduct[2]);
 
-    for (let j = 0; j<= 360; j+=stepBeta) {
-        for (let i = 0;  i<= 2 * b;  i+= stepAlpha){
-            x = calculateValue(i) * Math.cos(deg2rad(j));
-            y = calculateValue(i) * Math.sin(deg2rad(j));
-            z = i;
+
+            x = getX(i + 0.1, j);
+            y = getY(i + 0.1, j);
+            z = getZ(i + 0.1);
+            derU = getDerivativeU(i + 0.1, j, x, y, z, delta);
+            derV = getDerivativeV(i + 0.1, j, x, y, z, delta);
+            UVproduct = m4.cross(derU, derV);
             
             vertexList.push(x, y, z);
+            normalsList.push(UVproduct[0], UVproduct[1], UVproduct[2]);
         }
-        verticals++;
     }
 
-    return vertexList; 
+    return [vertexList, normalsList]; 
 }
 
 
@@ -218,11 +250,22 @@ function initGL() {
     shProgram.Use();
 
     shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal              = gl.getAttribLocation(prog,"normal");
+
+    shProgram.iWorldInverseTransposeLocation = gl.getUniformLocation(prog, "worldInverseTranspose");
+    shProgram.iLightWorldPositionLocation = gl.getUniformLocation(prog, "lightWorldPosition");
+    shProgram.iWorldLocation = gl.getUniformLocation(prog, "world");
+
+
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
     shProgram.iColor                     = gl.getUniformLocation(prog, "color");
 
     surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
+    let surfaceInfo = CreateSurfaceData()
+    let vertex = surfaceInfo[0];
+    let normals = surfaceInfo[1]
+
+    surface.BufferData(vertex, normals);
 
     gl.enable(gl.DEPTH_TEST);
 }
